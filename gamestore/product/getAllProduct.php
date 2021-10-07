03774bf9b2d8bc4aa1d7e9693e00633030737475
kennardvcs <kennard@sentosa.id>
2021-10-07 21:04:08 +0700

    error syntax

---
+++ gamestore/product/getAllProduct.php
@@ -0,0 +1,27 @@
+<?php
+    require("../setting.php");
+    $data = array();
+    $response = 0;
+    $sql = "SELECT * from product";
+    $res = mysqli_query($conn, $sql);
+
+    if($res) {
+        while($row = mysqli_fetch_array($res)) {
+            array_push($data,array(
+                'product_id' => $row["product_id"],
+                'product_name' => $row["product_name"],
+                'product_price' => $row["product_price"],
+                'product_img' => $row["product_img"],
+                'product_type' => $row["product_type"],
+                'stock' => $row["stock"]
+            ));
+        }
+        $response["value"] = 0;
+        $response["message"] = "berhasil";
+        $response["data"] = $data;
+    } else {
+        $response["value"] = 1;
+        $response["message"] = "error";
+    }
+    echo json_encode($response);
+?>
